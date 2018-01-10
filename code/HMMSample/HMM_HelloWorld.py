bfab633cbcc712e3f52e179062b25bd662627495
travis <traviszeng7@gmail.com>
2018-01-10 23:21:52 +0800

    todo: understand covariance

--- code/HMMSample/HMM_HelloWorld.py
+++ code/HMMSample/HMM_HelloWorld.py
@@ -3,18 +3,25 @@ import matplotlib.pyplot as plt
 
 from hmmlearn import hmm
 
+#初始状态空间的概率
 startprob = np.array([0.6, 0.3, 0.1, 0.0])
+
 # The transition matrix, note that there are no transitions possible
 # between component 1 and 3
+#转换概率矩阵 1和3 2和4之间不存在转换可能性
 transmat = np.array([[0.7, 0.2, 0.0, 0.1],
                      [0.3, 0.5, 0.2, 0.0],
                      [0.0, 0.3, 0.5, 0.2],
                      [0.2, 0.0, 0.2, 0.6]])
+
 # The means of each component
+#“means”用来表示各个隐藏状态对应的高斯分布期望向量μ形成的矩阵
+#第一类点的平均坐标为(0,0),etc
 means = np.array([[0.0,  0.0],
                   [0.0, 11.0],
                   [9.0, 10.0],
                   [11.0, -1.0]])
+
 # The covariance of each component
 covars = .5 * np.tile(np.identity(2), (4, 1, 1))
 
@@ -27,6 +34,7 @@ model.startprob_ = startprob
 model.transmat_ = transmat
 model.means_ = means
 model.covars_ = covars
+
 # Generate samples
 X, Z = model.sample(500)
 
